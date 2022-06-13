d1278de9e0e5af12973e1d281dd98c96db9768fa
ubongndoh <ubongndoh@gmail.com>
2022-06-13 10:00:58 -0700

    a function that prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,18 @@
+
+
+#include "holberton.h"
+
+/**
+ ** main - check the code
+ **
+ ** Return: Always 0.
+ **/
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. i fear the lack of them - isaac Asimov";
+	_puts(str);
+	return (0);
+}
