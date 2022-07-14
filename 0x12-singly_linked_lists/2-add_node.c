086a48b246837ba5b2c146ec862e2bfd036d2694
ubongndoh <ubongndoh@gmail.com>
2022-07-14 16:57:10 -0700

    fixed 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,7 +6,7 @@
  ** add_node - adds a new node at the beginning of a list_t list.
  ** @head: a double pointer to a list_t linked list
  * @str: string to be copied to the str member of head
- ** 
+ *
  ** Return: returns a pointer to newly created node.
  **/
 list_t *add_node(list_t **head, const char *str)
@@ -39,7 +39,7 @@ int _strlen(const char *s)
 	int i = 0;
 
 	while (*s++)
-		 i++;
-		    
+		i++;
+
 	return (i);
 }
