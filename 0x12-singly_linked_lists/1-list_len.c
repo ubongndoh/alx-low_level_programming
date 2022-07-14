c7af154d376ec79d2a7dafe9fb8d0b675a84ddf3
ubongndoh <ubongndoh@gmail.com>
2022-07-14 16:51:54 -0700

    fixed 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,7 +5,7 @@
 /**
  ** list_len - Determines the number of elements in a linked list_t list
  * @h: the list_t linked list.
- * 
+ *
  ** Return: the number of elements in h
  **/
 size_t list_len(const list_t *h)
