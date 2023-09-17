ebf3437bc78803b8a78649b75972188bb2b0eb22
ubongndoh <ubongndoh@gmail.com.com>
2023-09-17 12:23:17 -0700

    0x10-variadic_functions

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_all("ceis", 'B', NULL, 3, "stSchool");
+    return (0);
+}
